b76869a4bab1eacb446c72a9d4ace100ae52b2f0
Moneil97 <cam8059@yahoo.com>
2016-06-29 00:14:57 -0400

    init

---
+++ src/AnyBase.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class AnyBase {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Arrays.toString(convert(2056, 2, 20)));
+		System.out.println(Arrays.toString(convert(123456789, 255, 5)));
+		System.out.println(Arrays.toString(convert(0b1001100110010, 10, 5)));
+
+	}
+	
+	public static int[] convert(long original, int base, int maxSize) {
+		
+		int buf[] = new int[maxSize];
+        boolean negative = (original < 0);
+        int charPos = maxSize-1;
+		
+		if (base <= 1)
+			return buf;
+		
+        if (!negative)
+        	original = -original;
+        
+        while (original <= -base) {
+        	buf[charPos--] = (int) -(original % base);
+            original = original / base;
+        }
+        
+        buf[charPos] = (int) -original;
+        
+        return buf;
+    }
+}
